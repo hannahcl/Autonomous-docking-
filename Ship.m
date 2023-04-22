cd3652ac5d5e899961d229b427d6dbb90773ccc4
Hannah C Laugaland <hannahchaplin@hotmail.no>
2023-04-22 09:24:47 +0200

    linear model ctrl to zero

--- Ship.m
+++ Ship.m
@@ -3,7 +3,7 @@ classdef Ship
    properties
         m = 5; 
         Iz = 1; 
-        U = 10; %cruise speed
+        U = 10; 
         x_g = 1;
         X_u_dot = 1; 
         Y_u_dot = 1; 
@@ -20,17 +20,23 @@ classdef Ship
         C_A
         D
         N
+
         B
         C
         A
+
+        Q
+        R
+        K
+
         alpha
-
-        u = zeros(3,1); 
-
-        x0 = [10; 10; -10];
+ 
+        x0 
    end
    methods(Access = public)
       function obj = Ship()
+
+         %Matricies defining the linear model
          obj.M_RB = [obj.m 0 0; 0 obj.m obj.m*obj.x_g; 0 obj.m*obj.x_g obj.Iz];
          obj.MA = [obj.X_u_dot 0 0; 0 obj.Y_v obj.Y_r; 0 obj.N_v obj.N_r];
          obj.M = obj.M_RB + obj.MA; 
@@ -38,23 +44,42 @@ classdef Ship
          obj.C_A = [0 0 0; 0 0 -obj.X_u_dot*obj.U; 0 (obj.X_u_dot-obj.Y_u_dot)*obj.U -obj.Y_u_dot*obj.U];
          obj.D = -[obj.X_u 0 0; 0 obj.Y_v obj.Y_r; 0 obj.N_v obj.N_r];
          obj.N = obj.C_RB + obj.C_A + obj.D;
-         obj.A = -inv(obj.M)*obj.N; 
-         obj.B = [0; 1; 0]; 
-         obj.C = [0 -1 0]; 
+
+         %LInear model with normal notation
+         obj.A = -inv(obj.M)*obj.N;
+         obj.B = eye(3); 
+         obj.C = [0 -1 0];
+
+         %Nominal controler with feedforward
+         obj.Q = eye(3); 
+         obj.R = eye(3); 
+         obj.K = lqr(obj.A, obj.B, obj.Q, obj.R); 
+
+         %CBF controller
          obj.alpha = 1; 
+
+         %paramters
+         obj.x0 = [10; 10; -10];
+         
       end
 
       function u = controller(obj, x)
-        CB = obj.C*obj.B; 
-        nev = (obj.C*obj.A*x + obj.alpha*obj.C*x); 
-        u = nev/(-CB); 
+        %Linear model. No nominal control objective. Allowalbe set: y < 0. 
+%         CB = obj.C*obj.B; 
+%         nev = (obj.C*obj.A*x + obj.alpha*obj.C*x); 
+%         u = nev/(-CB); 
+          u = zeros(3,1); 
 
       end
 
-      function x_dot = model(obj, t, x)
-        obj.u = obj.controller(x);
-        x_dot = -obj.M\obj.N*x + obj.B*obj.u;
+      function x_dot = model(obj, t, x, u)
+        x_dot = obj.A*x + obj.B*u;
       end
+
+      function x_dot = closed_loop_model(obj, t, x, u)
+          x_dot = (obj.A-obj.B*obj.K)*x; 
+      end
+
    end
 
   methods
@@ -69,7 +94,7 @@ classdef Ship
         b = [1/6, 1/3, 1/3, 1/6]; 
 
         stages = size(b); stages = stages(2); 
-        sim_steps = round(T/ts); 
+        sim_steps = round(T/ts)-1; 
     
         x = zeros(3, sim_steps); 
         x(:, 1) = obj.x0; 
@@ -79,7 +104,8 @@ classdef Ship
             K = zeros(3,stages + 1); 
             sum_b = zeros(3,1); 
             for s = 1:stages
-                K(:, s+1) = obj.model(0, x(:, k) + ts*a(s)*K(:, s)); 
+                u = obj.controller(x(:, k)); 
+                K(:, s+1) = obj.closed_loop_model(0, x(:, k) + ts*a(s)*K(:, s), u); 
                 sum_b  = sum_b + b(s)*K(:,s+1); 
             end
     
@@ -89,11 +115,11 @@ classdef Ship
 
         t_arr = 0:ts:T; 
         subplot(1,3,1); 
-        plot(t_arr,x(1, 2:end), '-o')  
+        plot(t_arr,x(1, :), '-o')  
         hold on
-        plot(t_arr,x(2, 2:end), '-o')
+        plot(t_arr,x(2, :), '-o')
         hold on
-        plot(t_arr,x(3, 2:end), '-o')
+        plot(t_arr,x(3, :), '-o')
         hold off
         xlabel('Time')
         ylabel('State Variables')
